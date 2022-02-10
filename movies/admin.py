411e378cfda8f2828fbc30f6ebeedac1b2460bdd
Gustavo Mex <gustavoppymex@gmail.com>
2022-02-09 19:32:38 -0600

    CREATE MODEL IN MOVIES APP

--- movies/admin.py
+++ movies/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import MovieModel
+
+class MovieModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(MovieModel, MovieModelAdmin)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import UserModel
 
-# Create your models here.
+class MovieModel(models.Model):
+    usuario = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    review = models.TextField()
+    created = models.DateTimeField(auto_now_add=True),
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.usuario
+    
+    class Meta:
+        verbose_name = 'Movie'
+        verbose_name_plural = 'Movies'
+        ordering = ['-updated']
