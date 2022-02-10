e95dfcc2cda6f3adcb80edd5ab44d004087bf148
Gustavo Mex <gustavoppymex@gmail.com>
2022-02-09 19:31:47 -0600

    MODIFY AGE FIELD ** ADD VALIDATION TO AGE FIELD

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class UserModel(models.Model):
     GENDER = (
@@ -8,11 +9,8 @@ class UserModel(models.Model):
     )
     user_name = models.CharField(max_length=100)
     email = models.EmailField(blank=False, null=False)
-    edad = models.DecimalField(max_digits=2, decimal_places=0)
-    gender = models.CharField(
-        max_length=6,
-        choices=GENDER,
-    )
+    age = models.DecimalField(max_digits=2, decimal_places=0, validators=[MinValueValidator(0)])
+    gender = models.CharField( max_length=6, choices=GENDER)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
